1474c72edcef78fb82fcbd5395d0513c2830ae7f
gunsleobezerra <you@example.com>
2019-06-07 16:37:08 +0000

    improve C code

--- MatrixC.c
+++ MatrixC.c
@@ -4,25 +4,46 @@
 
 
 #define L 3
-#define rate 2
+#define rate 10
 
 void print_matrix2D(int matrix[][L],int sizeX,int sizeY);//A function to print one MATRIX 2D
 void random_squadmatrix(int matrix[][L],int sizeX,int sizeY);// initialize a matrix with random numbers
-void sum_matrix2D(int matrixA[][L],int matrixB[][L],int size);
-int **allocintmatrix2D(int size);
+void sum_matrix2D(int matrixA[][L],int matrixB[][L],int matrixresult[][L],int size); 
+void mult_matrix2D(int matrixA[][L],int matrixB[][L],int matrixresult[][L],int size);
+void convert_matrix2D(int **matrix,int matrixA[][L]);//converter a pointer matrix to matrix(array);
+void return_primary(int matrix[][L],int size);
 
 int main(){
     srand(time(NULL));
-    int A[L][L], B[L][L], C[L][L];
+    int A[L][L], B[L][L], C[L][L], matrixresult[L][L];
     
     //randomize matrix
+    //printf("Matrix A\n");
     random_squadmatrix(A,L,L);
-    print_matrix2D(A,L,L);
+    //print_matrix2D(A,L,L);
+
+    //printf("Matrix B\n");
     random_squadmatrix(B,L,L);
-    print_matrix2D(B,L,L);
+    //print_matrix2D(B,L,L);
+
+    //printf("Matrix C\n");
     random_squadmatrix(C,L,L);
-    print_matrix2D(C,L,L);
+    //print_matrix2D(C,L,L);
 
+    mult_matrix2D(A,C,matrixresult,L);
+    //print_matrix2D(matrixresult,L,L);
+    sum_matrix2D(matrixresult,B,matrixresult,L);
+
+    printf("(A X C + B) = \n");
+
+    print_matrix2D(matrixresult,L,L);
+    printf("primary diagonal\n");
+    return_primary(matrixresult,L);
+
+     printf("\n");
+
+    
+    
 
 
 
@@ -30,22 +51,7 @@ int main(){
 
 }
 
-int **allocintmatrix2D(int size){
 
-    int **matrix;
-    int i =0, j=0;
-
-        matrix = (int**)malloc(sizeof(int*)*size);
-        
-        for(i=0;i<size;i++){
-            *(matrix[i])=(int*)malloc(sizeof(int)*size);
-            
-        }
-    
-    
-
-
-}
 void print_matrix2D(int matrix[][L],int sizeX,int sizeY){
     
     int i=0,j=0;
@@ -56,7 +62,7 @@ void print_matrix2D(int matrix[][L],int sizeX,int sizeY){
 
             
 
-            printf(" %d ",matrix[i][j]);
+            printf(" %1d ",matrix[i][j]);
 
         }
 
@@ -82,16 +88,63 @@ void random_squadmatrix(int matrix[][L],int sizeX,int sizeY){
 
 }
 
-void sum_matrix2D(int matrixA[][L],int matrixB[][L],int size){
+void sum_matrix2D(int matrixA[][L],int matrixB[][L],int matrixresult[][L],int size){
 
      int i=0,j=0;
+
+    
 
     for(i=0;i<size;i++){
 
         for(j=0;j<size;j++){
 
+
+            matrixresult[i][j] = matrixA[i][j] + matrixB[i][j];
             
            
         }
     }
+}
+
+void mult_matrix2D(int matrixA[][L],int matrixB[][L],int matrixresult[][L],int size){
+    int i=0,j=0,k=0,sum=0;
+
+     for(i=0;i<size;i++){         
+        for (;k<size;k++){
+                  
+            for(j=0;j<size;j++){
+
+
+                sum += matrixA[i][j] * matrixB[j][k];
+                
+            
+            }
+            matrixresult[i][k] =sum;
+            sum=0; 
+        }
+        k=0;
+    }
+
+}
+
+void convert_matrix2D(int **matrix,int matrixA[][L]){
+    int i=0,j=0,size=L;
+    for(i=0;i<size;i++){
+
+        for(j=0;j<size;j++){
+
+
+             
+            
+           
+        }
+    }
+
+}
+
+void return_primary(int matrix[][L],int size){
+
+    int i=0;
+
+    for(;i<size;i++) printf(" %d ",matrix[i][i]);
 }
